0778a93bb1f9c3347e50445529fd24519075f0f9
SirmaXX <sea-97@hotmail.com>
2023-08-20 16:43:53 +0300

    testler eklendi

---
+++ tests/test_main.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient 
+
+from main import app
+client = TestClient(app)
+
+
+
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == True
+
+def test_predict():
+    response = client.post("/predict/", json={"features": [1,2,21,1,3]})
+    assert response.status_code == 200
+    assert response.json() == {"prediction": 2}
