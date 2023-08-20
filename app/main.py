6ba09952b20d8cb9126568bafb5ba1ea6c246623
SirmaXX <sea-97@hotmail.com>
2023-08-20 17:40:54 +0300

    config ayarları düzenlendi

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from .routers.items import items_router
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse,RedirectResponse
+
 
 def create_app():
-    app = FastAPI()
-    templates = Jinja2Templates(directory="app/templates")
+    app = FastAPI(title="Diabet indicator", description="Diabet indicator estimator with %86 accuracy example app and api") 
     app.include_router(items_router)
     return app
