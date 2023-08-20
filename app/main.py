dc797dd1c1b5d006acbd39f5ca4e1d33aa311aac
SirmaXX <sea-97@hotmail.com>
2023-08-20 16:15:50 +0300

    arayüz eklendi

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from .routers.items import items_router
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse,RedirectResponse
 
 def create_app():
     app = FastAPI()
+    templates = Jinja2Templates(directory="app/templates")
     app.include_router(items_router)
     return app
--- app/routers/items.py
+++ app/routers/items.py
@@ -1,15 +1,41 @@
-from fastapi import APIRouter,Request
+from fastapi import APIRouter,Request, Form
 from pydantic import BaseModel
 import joblib
 import numpy as np
-
+from fastapi.templating import Jinja2Templates
 import os
+
+templates = Jinja2Templates(directory="app/templates")
 
 model_path = os.path.join(os.path.dirname(__file__), '../../models/diabet_012.pkl')
 loaded_model = joblib.load(model_path)
 print(model_path)
 
 items_router = APIRouter()
+
+
+
+@items_router.get("/",description="index için router")
+async def api_index(request: Request):
+     return templates.TemplateResponse("index.html", {"request": request})
+
+
+
+@items_router.post('/predictform/',description="index için router")
+async def formpredict(
+    high_bp: int = Form(...),
+    high_collestrol: int = Form(...),
+    bmi: float = Form(...),
+    gen_health:int = Form(...),
+    diff_walk: int = Form(...),
+):
+    json= [high_bp,high_collestrol,bmi,gen_health,diff_walk]
+
+    features = np.array(json).reshape(1, -1)  # Ensure proper shape for prediction
+    prediction = loaded_model.predict(features)
+    return {"prediction": int(prediction[0])}
+
+
 
 
 
@@ -22,14 +48,6 @@ async def health(req: Request):
         return None
 
 
-
-@items_router.get("/",description="index için router")
-async def api_index():
-    """ 
-    iş servisinin giriş sayfası
-    """
-    return {"Hello": "Job"}
-
 class Item(BaseModel):
     features: list
 
@@ -38,3 +56,17 @@ def predict(item: Item):
     features = np.array(item.features).reshape(1, -1)  # Ensure proper shape for prediction
     prediction = loaded_model.predict(features)
     return {"prediction": int(prediction[0])}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
